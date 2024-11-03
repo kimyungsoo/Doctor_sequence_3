03556a9c4402a712ea8a20d56a98f5d8ba8918dd
kimyungsoo <149565656+kimyungsoo@users.noreply.github.com>
2024-11-03 17:09:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
-// server.js
 const WebSocket = require('ws');
+
 const server = new WebSocket.Server({ port:10000 });
 
 let currentChair = '선택되지 않음';
+let nextChairs = ['비어 있음', '비어 있음', '비어 있음'];
+
+function broadcastState() {
+  const state = {
+    currentChair,
+    nextChairs,
+  };
+  // 연결된 모든 클라이언트에게 현재 상태 전송
+  server.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(state));
+    }
+  });
+}
 
 server.on('connection', (ws) => {
-  console.log('클라이언트가 연결되었습니다.');
-
-  // 클라이언트에게 현재 상태를 전송
-  ws.send(JSON.stringify({ chair: currentChair }));
+  // 새 클라이언트가 연결되면 현재 상태를 전송
+  ws.send(JSON.stringify({ currentChair, nextChairs }));
 
   ws.on('message', (message) => {
     const data = JSON.parse(message);
-    currentChair = data.chair;
 
-    // 모든 클라이언트에게 새로운 상태를 전송
-    server.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ chair: currentChair }));
+    if (data.action === 'selectChair') {
+      // 다음 진료할 체어 목록에 새로운 체어 추가
+      for (let i = 0; i < nextChairs.length; i++) {
+        if (nextChairs[i] === '비어 있음') {
+          nextChairs[i] = data.chair;
+          break;
+        }
       }
-    });
-  });
+    } else if (data.action === 'endCurrentChair') {
+      // 현재 진료 중인 체어를 다음 체어로 바꾸고, 목록 이동
+      if (nextChairs[0] !== '비어 있음') {
+        currentChair = nextChairs[0];
+        nextChairs = [...nextChairs.slice(1), '비어 있음'];
+      } else {
+        currentChair = '선택되지 않음';
+      }
+    }
 
-  ws.on('close', () => {
-    console.log('클라이언트가 연결을 종료했습니다.');
+    // 업데이트된 상태를 모든 클라이언트에게 전송
+    broadcastState();
   });
 });
 
